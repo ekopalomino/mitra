b8a842ed097885f90474c96b6e29dc9d4fbd890b
EHW <heru.palomino@gmail.com>
2021-06-15 09:09:37 +0700

    Add Role Modules

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,12 +14,16 @@ class CreateUsersTable extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->bigIncrements('id');
+            $table->uuid('id');
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->uuid('status_id')->nullable();
+            $table->datetime('last_login_at')->nullable();
+            $table->string('last_login_from')->nullable();
             $table->rememberToken();
+            $table->primary('id');
             $table->timestamps();
         });
     }
